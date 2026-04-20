8a46ca22e2fb17133eb4c076bb5a8bd61d778ec3
Tanmayi Nagabhairava <tanmayinagabhairava@gmail.com>
2026-04-20 17:02:57 +0530

    Phase 4.2: Implement tree generation and parent commit lookup

--- commit.c
+++ commit.c
@@ -193,3 +193,15 @@ int head_update(const ObjectID *new_commit) {
 //   - head_update       : moves the branch pointer to your new commit
 //
 // Returns 0 on success, -1 on error.
+int commit_create(const char *message, ObjectID *commit_id) {
+    ObjectID tree_id;
+
+    // build tree
+    if (tree_from_index(&tree_id) != 0) {
+        return -1;
+    }
+
+    // get parent commit (if exists)
+    ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
+}
